f6d82aade1c6b5def5bcaa4122acb9ec100c1287
Goutam-Koturti <goutam.koturti@gmail.com>
2025-04-19 11:38:21 +0530

    Fixed the backspace issue

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import OtpInput from "./components/OtpInput/OtpInput";
+
+function App() {
+  return (
+    <div className="App">
+      <OtpInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/OtpInput/OtpInput.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./otp.css";
+
+function OtpInput() {
+  const [otpLength, setOtpLenth] = useState(6);
+  const [inputArray, setInputArray] = useState(new Array(otpLength).fill(""));
+  const refs = [useRef(), useRef(), useRef(), useRef(), useRef(), useRef()];
+
+  const handleChange = (e, index) => {
+    const value = e.key;
+
+    if (value === "Backspace") {
+      let copyArray = [...inputArray];
+      copyArray[index] = "";
+      setInputArray(copyArray);
+      if (index > 0) {
+        refs[index - 1].current.focus();
+      }
+    } else if (!isNaN(value)) {
+      let copyArray = [...inputArray];
+      copyArray[index] = value;
+      setInputArray(copyArray);
+      if (value && index < otpLength - 1) {
+        refs[index + 1].current.focus();
+      }
+    } else {
+      return;
+    }
+  };
+
+  useEffect(() => {
+    refs[0].current.focus();
+  }, []);
+
+  return (
+    <div>
+      <h1>Enter your OTP</h1>
+      <div>
+        {inputArray.map((_, i) => (
+          <input
+            value={inputArray[i]}
+            key={i}
+            type="text"
+            className="input"
+            onKeyDown={(e) => {
+              handleChange(e, i);
+            }}
+            ref={refs[i]}
+          ></input>
+        ))}
+      </div>
+      <button style={{ margin: "20px 0 0 0" }}>Submit</button>
+    </div>
+  );
+}
+
+export default OtpInput;
